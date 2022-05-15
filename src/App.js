611a770e2c3f0b7d1afdf9ec88fb0962126c4c45
Tomas Sanchez <tms90@outlook.es>
2022-05-16 01:56:30 +0200

    Added useDispacth and useEffect in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import RobotList from "./components/RobotList/RobotList";
+import { loadRobotDataThunk } from "./redux/thunks/thunks";
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadRobotDataThunk());
+  }, [dispatch]);
   return (
     <div className="App">
       {" "}
       <h1>Robots Web in process...</h1>
+      <RobotList />
     </div>
   );
 }
